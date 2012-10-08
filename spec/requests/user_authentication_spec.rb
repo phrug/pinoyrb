7ee323ded94df7f81513af9d324277023f3a5b70
Terence Ponce <terenceponce@gmail.com>
2012-10-08 14:23:58 +0800

    Improve authentication tests

--- spec/requests/user_authentication_spec.rb
+++ spec/requests/user_authentication_spec.rb
@@ -1,12 +1,25 @@
 require 'spec_helper'
 
 describe 'user authentication' do
-  before(:all) do
-    user = User.create(email: 'terence@example.com', password: 'password')
+  describe 'signing up' do
+    it 'should allow user to register with an email address and a password' do
+      visit root_path
+
+      click_link 'Sign up'
+
+      fill_in 'Email',                 with: 'test@example.com'
+      fill_in 'Password',              with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+
+      click_button 'Sign up'
+
+      page.should have_content('Welcome! You have signed up successfully.')
+    end
   end
 
   describe 'signing in' do
     it 'should allow users to sign in after they have registered' do
+      user = User.create(email: 'terence@example.com', password: 'password')
       visit root_path
 
       click_link 'Sign in'
@@ -22,6 +35,7 @@ describe 'user authentication' do
 
   describe 'signing out' do
     it 'should allow users to sign out when they are signed in' do
+      user = User.create(email: 'terence@example.com', password: 'password')
       visit root_path
 
       click_link 'Sign in'
