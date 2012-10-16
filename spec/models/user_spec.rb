f46484afa3ae0530291eee658a445ae473dd81e7
Terence Ponce <terenceponce@gmail.com>
2012-10-17 00:14:12 +0800

    Add user model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe User do
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_me) }
+end
