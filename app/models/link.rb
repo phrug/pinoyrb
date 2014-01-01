c81bcd7932231c927917f8fe679e65f2b3eaf882
Bryan Bibat <bry@bryanbibat.net>
2014-01-02 05:56:55 +0800

    SEO Tweaks, Add links to user profile

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,6 +3,7 @@ class Link < ActiveRecord::Base
   attr_accessible :link_type, :linkable_id, :linkable_type, :url
 
   LINK_TYPES = %w(website github facebook twitter linkedin gplus pinterest)
+  LINK_TYPE_FA_LOOKUP = Hash[LINK_TYPES.zip(%w(icon-external-link icon-github icon-facebook icon-twitter icon-linkedin icon-google-plus icon-pinterest))]
 
   validates :url, :presence => true
   validates :link_type, :inclusion => { :in => LINK_TYPES }, :presence => true
