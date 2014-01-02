9fc2c8eaa9752ae03afc4ec1425eb9e026d67e5c
Bryan Bibat <bry@bryanbibat.net>
2014-01-02 12:43:09 +0800

    Change ordering to alphabetical

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
   def index
     # very inefficient but not a problem considering the low volume of data
-    @users = User.includes(:presentations).all.sort_by do |user|
-      -user.presentations.count
-    end.reject { |user| user.presentations.count < 1 }
+    @users = User.includes(:presentations).order(:name).all
+      .reject { |user| user.presentations.count < 1 }
   end
 
   def show
