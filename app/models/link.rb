01bad7783334be1d15983500bab7011abcb492c5
Bryan Bibat <bry@bryanbibat.net>
2014-01-24 21:15:54 +0800

    Use new Font Awesome classes

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ActiveRecord::Base
   belongs_to :linkable, :polymorphic => true, :inverse_of => :links
 
   LINK_TYPES = %w(website github facebook twitter linkedin gplus pinterest)
-  LINK_TYPE_FA_LOOKUP = Hash[LINK_TYPES.zip(%w(icon-external-link-sign icon-github icon-facebook icon-twitter icon-linkedin icon-google-plus icon-pinterest))]
+  LINK_TYPE_FA_LOOKUP = Hash[LINK_TYPES.zip(%w(fa-icon-external-link-sign fa-icon-github fa-icon-facebook fa-icon-twitter fa-icon-linkedin fa-icon-google-plus fa-icon-pinterest))]
 
   validates :url, :presence => true
   validates :link_type, :inclusion => { :in => LINK_TYPES }, :presence => true
