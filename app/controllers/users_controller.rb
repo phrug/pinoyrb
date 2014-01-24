71c99e1511e9cb852d120509e4327d1a367046c6
Bryan Bibat <bry@bryanbibat.net>
2014-01-24 21:05:04 +0800

    Add users with no videos. Kasi love nami si Papa Rico

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def index
     # very inefficient but not a problem considering the low volume of data
     @users = User.includes(:presentations).order(:name).load
-      .reject { |user| user.presentations.count < 1 }
   end
 
   def show
