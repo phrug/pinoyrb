4b57f7017f1a89eb432d00239de20046a17b50d7
Bryan Bibat <bry@bryanbibat.net>
2014-05-26 11:24:52 +0800

    Travis: Remove test:prepare, Remove 1.9

--- db/migrate/20130401080427_add_correct_polyorphic_fields_to_contributor.rb
+++ db/migrate/20130401080427_add_correct_polyorphic_fields_to_contributor.rb
@@ -1,11 +1,11 @@
 class AddCorrectPolyorphicFieldsToContributor < ActiveRecord::Migration
   def change
-    rename_column :contributors, :contributor_id, :contributable_id
-    rename_column :contributors, :contributor_type, :contributable_type
-    
     remove_index :contributors, :contributor_type
     remove_index :contributors, :contributor_id
 
+    rename_column :contributors, :contributor_id, :contributable_id
+    rename_column :contributors, :contributor_type, :contributable_type
+    
     add_index :contributors, :contributable_type
     add_index :contributors, :contributable_id
   end
